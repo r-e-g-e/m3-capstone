69c6c053455f88b9a8c431aefad78e446ae62aee
wellery <wellerychaves@outlook.com>
2022-03-16 19:42:29 -0300

    feat(sistema de cores): criado componentes de circulo para sistema de cores

---
+++ src/components/Circulos/index.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { CirculosSistemasDeCores } from "./styles";
+
+function CirculoSistemaCores({ color }) {
+  return <CirculosSistemasDeCores color={color}></CirculosSistemasDeCores>;
+}
+
+export default CirculoSistemaCores;
