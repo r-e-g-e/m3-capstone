5dcaacc974b5d37e81f2ae5a0929deb1e34e7f1f
Rodrigo Tavares <rodrigotvrs70@hotmail.com>
2022-03-16 10:33:52 -0300

    feat: react-router-dom estrutura base

---
+++ src/app.jsx
@@ -0,0 +1,14 @@
+import { Route, Switch } from "react-router-dom";
+
+function App() {
+  return (
+    <>
+      <Switch>
+        <Route exact path={"/"}>
+        </Route>
+      </Switch>
+    </>
+  );
+}
+
+export default App;
