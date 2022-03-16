b598bdf90c30c7b7521fb7e42f45b01eeb6c1ae4
Raphael Macedo <macedolima22@hotmail.com>
2022-03-16 14:20:55 -0300

    Primeiro componente feito

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import GlobalStyle from "./styles/global";
+import ButtonHeader from "./components/ButtonHeader";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ButtonHeader value={"button"} />
     </>
   );
 }
---
+++ src/components/ButtonHeader/styles.js
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+export const Container = styled.button`
+  background: var(--branco);
+  color: var(--laranja);
+  padding: 5px;
+  border-color: var(--branco);
+  border-radius: 5px;
+  :hover {
+    background: var(--laranja);
+    color: var(--branco);
+  }
+`;
