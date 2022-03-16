7b9558a225aa35f14eee523e022675c758bdee0e
wellery <wellerychaves@outlook.com>
2022-03-16 19:46:08 -0300

    feat(pagina sobre): pagina sobre mobile praticamente finalizada

--- src/pages/sobre/index.jsx
+++ src/pages/sobre/index.jsx
@@ -1,12 +1,62 @@
 import React from "react";
-import { Container } from "./styles";
+import CirculoSistemaCores from "../../components/Circulos";
 import Header from "../../components/Header";
+import PerfilCriadores from "../../components/PerfilCriadores";
+import { DivCriadores, DivSistemaCores } from "./styles";
+import {
+  Container,
+  ContentContainer,
+  TitulosLightItalic,
+  PDescricao,
+} from "./styles";
 
 function PaginaSobre() {
-
   return (
     <Container>
-      <Header/>
+      {/* Header da página */}
+      <Header />
+      <ContentContainer>
+        {/* Objetivo */}
+        <TitulosLightItalic>Qual é o nosso objetivo?</TitulosLightItalic>
+        <PDescricao>
+          Lorem ipsum, dolor sit amet consectetur adipisicing elit. Debitis ad
+          alias ipsum veniam iusto minus ex, placeat voluptatibus minima sint,
+          recusandae accusantium voluptatum? Provident nihil dolorum, quaerat
+          eos explicabo voluptates.
+        </PDescricao>
+
+        {/* Sistema de cores */}
+        <TitulosLightItalic>Entenda o sistema de cores:</TitulosLightItalic>
+        <DivSistemaCores>
+          <CirculoSistemaCores color="verde" />
+          <p>
+            Verde: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </DivSistemaCores>
+        <DivSistemaCores>
+          <CirculoSistemaCores color="amarelo" />
+          <p>
+            Amarelo: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </DivSistemaCores>
+        <DivSistemaCores>
+          <CirculoSistemaCores />
+          <p>
+            Vermelho: Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+          </p>
+        </DivSistemaCores>
+
+        {/* Controle e criação do ponto de coleta */}
+        <TitulosLightItalic>
+          Controle e criação do ponto de coleta
+        </TitulosLightItalic>
+
+        {/* Criadores do Ajude.me */}
+        <TitulosLightItalic>Criadores do Ajude.me</TitulosLightItalic>
+        <DivCriadores>
+          <PerfilCriadores />
+        </DivCriadores>
+      </ContentContainer>
     </Container>
   );
 }
--- src/pages/sobre/styles.js
+++ src/pages/sobre/styles.js
@@ -1,9 +1,39 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-
+display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+export const ContentContainer = styled.div`
+  width: 90%;
 `;
 
-export const Header = styled.header`
+export const TitulosLightItalic = styled.h2`
+  font-size: 18px;
+  font-weight: 500;
+  font-style: italic;
+  font-family: 'Roboto', sans-serif;
+  color: var(--laranja);
+  text-align: center;
+  margin-top: 20px;
+`;
+
+export const PDescricao = styled.p`
+  color:var(--laranja);
+  font-family: 'Roboto', sans-serif;
+`;
+
+export const DivSistemaCores = styled.div`
+  display: flex;
+  margin-top: 10px;
+  color: var(--laranja);
+`;
+
+export const DivCriadores = styled.div`
+  margin-top: 20px;
+  display: flex;
+  justify-content: space-around;
   
 `;
