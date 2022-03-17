d0fc7c95ab2a06dde18aad74a55452b4da187ff7
Raphael Macedo <macedolima22@hotmail.com>
2022-03-16 22:51:59 -0300

    feature conforme a development

--- src/app.jsx
+++ src/app.jsx
@@ -1,12 +1,10 @@
 import { Route, Switch } from "react-router-dom";
 import GlobalStyle from "./styles/global";
-import CardPerfil from "./components/CardPerfil";
 
 function App() {
   return (
     <>
       <GlobalStyle />
-      <CardPerfil />
       <Switch>
         <Route exact path={"/"}></Route>
       </Switch>
