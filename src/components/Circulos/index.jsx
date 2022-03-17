7af02a08b173f22de7bd42101235ba61be5e3fb7
wellery <wellerychaves@outlook.com>
2022-03-17 15:10:57 -0300

    docs(comentario): comentario explicando objetivo do componente

--- src/components/Circulos/index.jsx
+++ src/components/Circulos/index.jsx
@@ -1,5 +1,6 @@
 import { CirculosSistemasDeCores } from "./styles";
 
+// Circulos da pagina sobre, são das cores que indicam qual a importancia do card
 function CirculoSistemaCores({ color }) {
   return <CirculosSistemasDeCores color={color}></CirculosSistemasDeCores>;
 }
