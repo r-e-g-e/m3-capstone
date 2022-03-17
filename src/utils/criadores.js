374c996439bf06b048af007bbdf4bf127b7a6af5
wellery <wellerychaves@outlook.com>
2022-03-17 15:23:04 -0300

    feat(criadores): criado uma variavel que contem informações dos criadores em pasta separada para manter codigo limpo

---
+++ src/utils/criadores.js
@@ -0,0 +1,40 @@
+const criadores = [
+  {
+    nome: "Rodrigo Tavares",
+    foto: "RT",
+    cargo: "Tech Leader",
+    linkedin: "https://linkedin.com/in/rodrigo-t-lima/",
+  },
+  {
+    nome: "Bryan Torres",
+    foto: "BT",
+    cargo: "Scrum Master",
+    linkedin: "https://linkedin.com/in/bryan-torres-ribeiro-20a0531a8/",
+  },
+  {
+    nome: "Raphael Felipe",
+    foto: "RF",
+    cargo: "Product Owner",
+    linkedin: "https://linkedin.com/in/raphael-felipe-1310",
+  },
+  {
+    nome: "Junior da Silva",
+    foto: "JS",
+    cargo: "Quality Assurance",
+    linkedin: "http://linkedin.com/in/junior-da-silva",
+  },
+  {
+    nome: "Wellery Chaves",
+    foto: "WC",
+    cargo: "Quality Assurance",
+    linkedin: "https://linkedin.com/in/wellerychaves/",
+  },
+  {
+    nome: "Antônio Marcos",
+    foto: "AM",
+    cargo: "Quality Assurance",
+    linkedin: "https://linkedin.com/in/ant%C3%B4nio-marcos-152691218/",
+  },
+];
+
+export default criadores;
