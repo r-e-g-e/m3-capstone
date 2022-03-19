a5336fb9e6ee8159acc7d4cb630f67a551b6802b
Raphael Macedo <macedolima22@hotmail.com>
2022-03-19 17:48:19 -0300

    feat : CardPerfil estruturado conforme pedido

--- src/components/CardPerfil/index.jsx
+++ src/components/CardPerfil/index.jsx
@@ -2,27 +2,14 @@ import { Container } from "./styles";
 import Button from "../Button";
 import CardPontoColeta from "../CardPontoColeta";
 
-function CardPerfil({ Cor }) {
+function CardPerfil() {
   return (
     <>
       <Container>
-        <header>
-          <span className="perfil">
-            <span>
-              <h3>Pica-Pau</h3>
-              <img
-                className="mobile"
-                src={process.env.PUBLIC_URL + "/assets/picapau.jpg"}
-                alt="foto"
-              />
-            </span>
-          </span>
-          {/* <img
-          className="desktop"
-          src={process.env.PUBLIC_URL + "/assets/picapau.jpg"}
-          alt="foto"
-        /> */}
-        </header>
+        <section>
+          <h3>Pica-Pau</h3>
+          <img src={"/assets/picapau.jpg"} alt="foto" />
+        </section>
         <div className="ponto">
           <p>Você não possui nenhum ponto de coleta!</p>
           <Button>CRIAR PONTO DE COLETA</Button>
