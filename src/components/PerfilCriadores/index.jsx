73e8eea02500ea276a5f2b0bb281d9ef5490e392
wellery <wellerychaves@outlook.com>
2022-03-18 23:23:28 -0300

    refactor(criadores): codigo refatorado para ficar mais fiel ao figma

--- src/components/PerfilCriadores/index.jsx
+++ src/components/PerfilCriadores/index.jsx
@@ -1,19 +1,29 @@
-import { LinkedInLink } from "../../pages/sobre/styles";
-import { CargoSpan, Container, FotoPerfil } from "./styles";
+import {
+  Container,
+  Content,
+  Nome,
+  CargoSpan,
+  FotoPerfil,
+  LinkedInLink,
+} from "./styles";
 import criadores from "../../utils/criadores";
 
 function PerfilCriadores() {
   return (
     <>
       {criadores.map((pessoa, index) => (
-        <Container key={index}>
-          <FotoPerfil>{pessoa.foto}</FotoPerfil>
-          <p>{pessoa.nome}</p>
-          <CargoSpan>{pessoa.cargo}</CargoSpan>
-          <LinkedInLink href={pessoa.linkedin} target="_blank">
-            linkedin
-          </LinkedInLink>
-        </Container>
+        <>
+          <Container>
+            <Nome>{pessoa.nome}</Nome>
+            <Content key={index}>
+              <FotoPerfil src={pessoa.foto} alt={pessoa.nome} />
+              <CargoSpan>{pessoa.cargo}</CargoSpan>
+              <LinkedInLink href={pessoa.linkedin} target="_blank">
+                <img src="/assets/linkedInLogo.png" />
+              </LinkedInLink>
+            </Content>
+          </Container>
+        </>
       ))}
     </>
   );
--- src/components/PerfilCriadores/styles.js
+++ src/components/PerfilCriadores/styles.js
@@ -1,29 +1,58 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  width: 110px;
-  display: flex;
-  flex-direction: column;
-  justify-content: space-around;
-  align-items: center;
+  width: 100%;
 
-  p {
-    margin-top: 5px;
-    text-align: center;
-    font-weight: bold;
-  }
 
   @media only screen and (min-width: 600px) {
-    p {
-    margin-top: 10px;
-    font-size: 20px;
-    }
+    width: 110px;
+  }
+
+  @media only screen and (min-width: 1280px) {
+    width: 150px;
   }
 `;
 
-export const FotoPerfil = styled.div`
-  width: 50px;
-  height: 50px;
+export const Content = styled.div`
+  display: flex;
+  align-items: center;  
+  flex-direction: row;
+  justify-content: space-around; 
+
+  @media only screen and (min-width: 600px) {
+    flex-direction: column;
+  }
+`;
+
+export const Nome = styled.p`
+  width: 100%;
+  height: 36px;
+  margin-top: 5px;
+  margin-bottom: 10px;
+  font-weight: 500;
+  font-style: italic;
+  text-align: center;
+  color: var(--laranja);
+  font-family: 'Roboto', sans-serif;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+    @media only screen and (min-width: 600px) {
+      margin-top: 10px;
+      font-size: 16px;
+    }
+
+    @media only screen and (min-width: 1280px) {
+      width: 150px;
+      font-size: 20px;
+    }
+`;
+
+export const FotoPerfil = styled.img`
+  width: 100px;
+  height: 100px;
   border-radius: 100%;
   color: var(--branco);
   background-color: var(--laranja);
@@ -32,15 +61,55 @@ export const FotoPerfil = styled.div`
   align-items: center;
   font-size: 30px;
 
+
+  
   @media only screen and (min-width: 600px) {
-  width: 110px;
-  height: 110px;
+    width: 90px;
+    height: 90px;
+    margin-bottom: 10px;
+  }
+
+  @media only screen and (min-width: 800px) {
+    width: 110px;
+    height: 110px;
+  }
+
+  @media only screen and (min-width: 1280px) {
+    width: 150px;
+    height: 150px;
   }
 `;
 
 export const CargoSpan = styled.span`
-  text-align: center;
+  height: 36px;
+
   font-size: 14px;
-  opacity: 0.8;
-  margin: 5px 0;
+  font-weight: 500;
+  font-style: italic;
+  text-align: center;
+  color: var(--laranja);
+  margin: 0;
+
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  @media only screen and (min-width: 1280px) {
+    width: 100%;
+  }
+  @media only screen and (min-width: 1280px) {
+      width: 150px;
+      font-size: 16px;
+    }
+`;
+
+export const LinkedInLink = styled.a`
+  cursor: pointer;
+  margin-top: 10px;
+  text-align: center;
+  color: var(--verde-escuro);
+  
+  img {
+    width: 30px;
+  }
 `;
