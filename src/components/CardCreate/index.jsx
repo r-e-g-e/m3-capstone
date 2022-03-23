f25257112299c7d67679787c1bd3d4b4b9436bbb
wellery <wellerychaves@outlook.com>
2022-03-23 14:43:59 -0300

    feat(CardCreate): adicionado função que apresenta e remove modal

--- src/components/CardCreate/index.jsx
+++ src/components/CardCreate/index.jsx
@@ -1,13 +1,25 @@
 import { Container } from "./styles";
 import Button from "../Button";
 
-function CardCreate() {
+function CardCreate({ setShowModal }) {
+  const handleSubmit = () => {
+    setShowModal(true);
+  };
+
   return (
     <>
       <Container>
         <div className="ponto">
           <p>Você não possui nenhum ponto de coleta!</p>
-          <Button width={`${200}px`} height={`${40}px`} bgColor = {"orange"} >CRIAR PONTO DE COLETA</Button>
+          <Button
+            onClick={handleSubmit}
+            width={`${200}px`}
+            height={`${40}px`}
+            bgColor={"orange"}
+            type="button"
+          >
+            CRIAR PONTO DE COLETA
+          </Button>
         </div>
       </Container>
     </>
